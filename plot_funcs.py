b33637ed1fb64e4026fd657ce59abcb1e5874ee0
Nicholas Sotiriou <nsotiriou88@users.noreply.github.com>
2022-01-05 11:33:39 +0000

    [add] added cv_results plotting function for GridSearchCV plot

--- plot_funcs.py
+++ plot_funcs.py
@@ -540,3 +540,103 @@ def ABS_SHAP(df_shap,df):
     ax.set_xlabel("SHAP Value (Red = Positive Impact)")
 
     return
+
+
+def plot_cv_scores(cv_results, param, scoring, xlim=None, ylim=None, grid=False,
+                   loc='best', figsize=(16, 9)):
+    '''
+    This function plots scores from cross validation results.
+
+    This function can plot the results of 'GridSearchCV.cv_results_', when used
+    to validate ONE parameter only. For example, in a Tree-based:
+
+    parameters = {
+        'n_estimators': range(100, 2501, 100),
+        'criterion': ['gini'],
+        'max_depth': [8],
+        'min_samples_split': [2],
+        'min_samples_leaf': [1],
+        'max_ features': [10],
+        'class_ weight': [{0:1, 1:round(ratio)}],
+        'n_jobs': [-1],
+        'max_samples': [0.85]
+    }
+
+    NOTE that we tested only 'n_estimators' in this example.
+
+    Parameters
+    ----------
+    cv_results: dict
+    Dictionary obtained after the '.fit()' method on 'GridSearchCV' object by
+    calling the attribute '.cv_results_'. NOTE that you must set parameter
+    'return_train_score' to 'True' in order to get the output of the train score
+    and compare it.
+
+    param: str
+    The parameter that was used to validate through 'GridSearchCV'. It is used
+    on the x-axis.
+
+    scoring: str or list
+    List of scoring methods that have been used during the 'GridSearchCV', eg.
+    ['accuracy', 'f1']. All scores are displayed on y-axis.
+
+    xlim, ylim: tuple, default None
+    Control the limits of x-axis and y-axis. If None, matplotlib will use
+    default.
+
+    grid: bool, default False
+    Use grid in plot.
+
+    loc: str or None, default 'best'
+    Define position of legend location. if None, legend will not be diplayed.
+
+    figsize: tuple, default: (16, 9)
+    Matplotlib figure size.
+    '''
+    if type(scoring)!=list:
+        scoring = [scoring]
+    plt.figure(figsize=figsize)
+    plt.title("GridSearchCV evaluating using multiple scorers simultaneously",
+              fontsize=16)
+    plt.xlabel(param)
+    plt.ylabel("Score")
+    ax = plt.gca()
+    if xlim:
+        ax.set_xlim(xlim[0], xlim[1])
+    if ylim:
+        ax.set_ylim(ylim[0], ylim[1])
+        y_mark = ylim[0]
+    else:
+        y_mark = 0
+    # Get the regular numpy array from the MaskedArray
+    X_axis = np.array(cv_results['param_'+param].data, dtype=float)
+    for scorer, color in zip(sorted(scoring), ['g', 'k', 'r', 'b', 'm', 'y']):
+        for sample, style in (('train', '--'), ('test', '-')):
+            sample_score_mean = cv_results['mean_%s_%s' % (sample, scorer)]
+            sample_score_std = cv_results['std_%s_%s' % (sample, scorer)]
+            ax.fill_between(X_axis, sample_score_mean-sample_score_std,
+                            sample_score_mean+sample_score_std,
+                            alpha=0.1 if sample=='test' else 0, color=color)
+            ax.plot(X_axis, sample_score_mean, style, color=color,
+                    alpha=1 if sample=='test' else 0.7,
+                    label="%s (%s)" % (scorer, sample))
+        best_index = np.nonzero(cv_results['rank_test_%s' % scorer] == 1)[0][0]
+        best_score = cv_results['mean_test_%s' % scorer][best_index]
+        # Plot a dotted vertical line at the best score for that scorer
+        # marked by x.
+        ax.plot([X_axis[best_index], ] * 2, [y_mark, best_score],
+                linestyle='-.', color=color, marker='x', markeredgewidth=3,
+                ms=8)
+        # Annotate the best score for that scorer
+        ax.annotate("%0.2f" % best_score,
+                    (X_axis[best_index], best_score + 0.005))
+    if xlim:
+        ax.set_xlim(xlim[0], xlim[1])
+    if ylim:
+        ax.set_ylim(ylim[0], ylim[1])
+    plt.legend(loc=loc)
+    plt.grid(grid)
+    plt.show()
+
+    return
+
